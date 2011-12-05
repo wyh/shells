db978612ad15e5a70ff82fc23208c0fb6d8a9164
Samuel <wyh@shanbay.com>
2011-12-05 23:30:31 +0800

    updated pass_gfw to support many different walls inland or abroad

--- pass_gfw.sh
+++ pass_gfw.sh
@@ -1,2 +1,30 @@
-#ssh -fCqND 8964 samuel@blog.rizi.me
-ssh -fCqND 8964  beeblio@overwall.shanbay.com
+if [ -z $2 ]
+then
+    port=8964;
+else
+    port=$2
+fi
+    
+
+if [ -z $1 ]; then 
+    domain=beeblio@overwall.shanbay.com
+elif [ $1 ==  shanbay ]; then
+    domain=beeblio@overwall.shanbay.com
+
+elif [ $1 == xiaobo ]; 
+then
+    domain=beeblio@xiaobo;
+elif [ $1 == rizi ]; 
+then
+    domain=samuel@rizi.me
+fi
+
+echo $domain $port
+
+kill `ps -ef |grep "ssh -fCqND"|grep $port |awk '{print $2}'|head -1`
+
+ssh -fCqND $port  $domain
+
+if  [ $? -eq 0 ]; then 
+    echo "successfully connected!"
+fi 
